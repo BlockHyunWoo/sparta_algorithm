22ed7befda1ca3b53df411a5e889b9e4b1d2aa05
FintechHyun <33017374+BlockHyunWoo@users.noreply.github.com>
2021-07-04 15:49:09 +0900

    Initial commit

---
+++ Week_2/01_print_all_linkedlist.py
@@ -0,0 +1,44 @@
+# [3] -> [4]
+# data, next
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+node = Node(3)
+first_node = Node(4)
+node.next = first_node
+
+
+class LinkedList:
+    def __init__(self, data):
+        self.head = Node(data)
+
+    def append(self, data):
+        if self.head is None:
+            self.head = Node(data)
+            return
+
+        self.head.next = Node(data)
+        cur = self.head
+
+        while cur.next is not None:
+            cur = cur.next
+        cur.next = Node(data)
+        print("cur is", cur.data)
+
+    def print_all(self):
+        cur = self.head
+        while cur is not None:
+            print(cur.data)
+            cur = cur.next
+
+
+# [3] -> [4] -> [5] -> [6] -> [new]
+linked_list = LinkedList(3)
+linked_list.append(12)
+linked_list.append(8)
+linked_list.print_all()
+
+
---
+++ Week_2/07_is_existing_target_number_binary.py
@@ -0,0 +1,14 @@
+finding_target = 2
+finding_numbers = [0, 3, 5, 6, 1, 2, 4]
+
+def is_exist_target_number_binary(target, numbers):
+
+    for number in array:
+        if target == number:
+            return True
+    # 이 부분을 채워보세요!
+    return False
+
+
+result = is_exist_target_number_binary(finding_target, finding_numbers)
+print(result)
